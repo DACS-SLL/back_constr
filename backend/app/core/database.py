a4c43da1a0359cec9e59d40776b7223056101979
DACS <dcasass@ulasalle.edu.pe>
2025-07-03 19:16:07 -0500

    Finalizando back_database V1.0

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,12 +1,18 @@
-# app/core/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.engine.url import make_url
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Añadir client_encoding a la URL si no está presente
+url = make_url(settings.DATABASE_URL)
 
+# Solo agrega el parámetro si no existe
+if "client_encoding" not in url.query:
+    url = url.set(query={**url.query, "client_encoding": "utf8"})
+
+engine = create_engine(url, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
